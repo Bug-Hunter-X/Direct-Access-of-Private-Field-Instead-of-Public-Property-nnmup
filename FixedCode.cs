4f7036a2ced2d8d553cbf5e61849de6e576893f8
BugHunter-X <askzenai@gmail.com>
2025-02-08 02:22:05 +0100

    Add FixedCode.cs file

---
+++ FixedCode.cs
@@ -0,0 +1,16 @@
+public class ExampleClass
+{
+    private int _myField;
+
+    public int MyProperty
+    {
+        get { return _myField; }
+        set { _myField = value; }
+    }
+
+    public void MyMethod()
+    {
+        // Correctly accessing and modifying the field via the property
+        MyProperty = 10;
+    }
+}
